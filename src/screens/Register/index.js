3baafe717e39e270fac313495e38511959820513
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2022-11-14 15:05:18 +0700

    Add Card Student In Card Screen #S002

---
+++ src/screens/Register/index.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { View, Image, ScrollView } from 'react-native';
+import { Text, CustomTextInput, Button } from 'components/';
+import Styles from './style';
+import Images from 'consts/Images';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import form from 'helpers/form';
+import navigation from 'helpers/navigation';
+import screenName from 'config/screenName';
+import { Colors } from 'src/utils/colors';
+
+const Register = () => {
+
+  const registerValidationSchema = Yup.object().shape({
+    phone: form.phonePattern,
+    name: form.namePattern,
+    pin: form.pinPattern,
+    rewritePin: form.pinPattern,
+  });
+
+  return (
+    <View style={ Styles.container }>
+      { /* <KeyboardAvoidingView behavior='position' keyboardVerticalOffset={ keyboardVerticalOffset }> */ }
+      <View style={ Styles.containerLogo }>
+        <Image source={ Images.iconName } style={ Styles.logo } />
+      </View>
+      <ScrollView showsVerticalScrollIndicator={ false }>
+        <View style={ Styles.mainContainer }>
+          <Text weight='bold' size='xl' >
+          Register
+          </Text>
+          <Formik
+            initialValues={ {
+              phone: '+62',
+              name: '',
+              pin: '',
+              rewritePin:'',
+            } }
+            validationSchema={ registerValidationSchema }
+            onSubmit={ () => navigation.navigate(screenName.MAIN) }>
+            { ({
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              values,
+              errors,
+              touched,
+              isValid,
+            }) => (
+              <>
+                <CustomTextInput
+                  placeholder='No HP'
+                  onChangeText={ handleChange('phone') }
+                  onBlur={ handleBlur('phone') }
+                  keyboardType={ 'phone-pad' }
+                  value={ values.phone }
+                />
+                { errors.phone && touched.phone && (
+                  <Text size='xxs' color={ Colors.error }>{ errors.phone }</Text>
+                ) }
+                <CustomTextInput
+                  placeholder='Nama Lengkap'
+                  onChangeText={ handleChange('name') }
+                  onBlur={ handleBlur('name') }
+                  value={ values.name }
+                />
+                { errors.name && touched.name && (
+                  <Text size='xxs' color={ Colors.error }>{ errors.name }</Text>
+                ) }
+                <CustomTextInput
+                  placeholder='PIN'
+                  onChangeText={ handleChange('pin') }
+                  onBlur={ handleBlur('pin') }
+                  value={ values.pin }
+                  keyboardType={ 'number-pad' }
+                  isPassword
+                />
+                { errors.pin && touched.pin && (
+                  <Text size='xxs' color={ Colors.error }>{ errors.pin }</Text>
+                ) }
+                <CustomTextInput
+                  placeholder='Ulangi PIN'
+                  onChangeText={ handleChange('rewritePin') }
+                  onBlur={ handleBlur('rewritePin') }
+                  value={ values.rewritePin }
+                  keyboardType={ 'number-pad' }
+                  isPassword
+                />
+                { errors.rewritePin && touched.rewritePin && (
+                  <Text size='xxs' color={ Colors.error }>{ errors.rewritePin }</Text>
+                ) }
+                { values.rewritePin.length === 6 && values.rewritePin !== values.pin  && (
+                  <Text size='xxs' color={ Colors.error }>PIN harus sama</Text>
+                )
+                }
+                <Button
+                  text='Daftar'
+                  mode='default'
+                  style={ Styles.btn }
+                  onPress={ handleSubmit }
+                />
+              </>
+            ) }
+          </Formik>
+        </View>
+      </ScrollView>
+        
+      { /* </KeyboardAvoidingView> */ }
+    </View>
+  );
+};
+
+export default Register;
---
+++ src/screens/Register/style.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+import { Colors } from 'src/utils/colors';
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+  },
+  containerLogo: {
+    borderBottomRightRadius: 50,
+    borderBottomLeftRadius: 50,
+    paddingVertical: 30,
+    // backgroundColor: Colors.primary,
+  },
+  containerRegister: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 5,
+  },
+  logo: {
+    width: 200,
+    height: 80,
+    resizeMode: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  mainContainer: {
+    padding: 32,
+  },
+  btn: {
+    marginTop: 50,
+  },
+});
+
+export default Styles;
